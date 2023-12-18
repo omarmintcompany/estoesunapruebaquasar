9e890ba0c60a0c92485b8c1a455c425e62c14c6f
omarmintcompany <omarrodrigueztrujillo@gmail.com>
2023-12-18 14:51:33 +0000

    test

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>Quasar CLI with Vite App</q-toolbar-title>
+        <q-toolbar-title>Prueba Online Quasar</q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
